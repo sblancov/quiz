3e39aaec686fe3992fe0d8eb7ac62476357d5092
Santiago Blanco <sblancov84@gmail.com>
2015-07-03 20:21:52 +0200

    Add author page

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+var title = 'Quiz';
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { 'title': title });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', function (req, res, next) {
+    res.render('author', { 'title': title });
+});
 
 module.exports = router;
