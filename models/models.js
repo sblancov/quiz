94dbbbd1585a68df5f9073ef813e79c15b833d3e
Santiago Blanco <sblancov84@gmail.com>
2015-07-04 10:27:07 +0200

    Update models to use heroku

--- models/models.js
+++ models/models.js
@@ -1,12 +1,25 @@
 var path = require('path');
 
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DBname = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 var Sequelize = require('sequelize');
 
-var sequelizeOptions = {
-    dialect: 'sqlite',
-    storage: 'quiz.sqlite'
-};
-var sequelize = new Sequelize(null, null, null, sequelizeOptions);
+var sequelize = new Sequelize(DBname, user, pwd, {
+    dialect: dialect,
+    protocol: protocol,
+    port: port,
+    host: host,
+    storage: storage,
+    omitNull: true
+});
 
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
